0023f89edc94d44a52cd34edd40bff530b07e510
Anna Becker and Daren Schaad <anna.r.becker@gmail.com>
2016-02-16 08:21:47 -0800

    Add initial files.

---
+++ src/main/java/App.java
@@ -0,0 +1,11 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+import static org.fluentlenium.core.filter.FilterConstructor.*;
+
+
+public class App {
+  public static void main(String[] args) {}
+  }
